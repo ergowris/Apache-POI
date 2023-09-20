d191f0c4490228b6b88c7d807cbb522209f6be3a
Gowri <Gowri@DESKTOP-AF33508.Dlink>
2023-09-20 17:11:15 +0530

    created apache poi project

---
+++ Apache-POI/src/test/java/API/Apache_POI/AppTest.java
@@ -0,0 +1,20 @@
+package API.Apache_POI;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
